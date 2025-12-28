23132c9fcdf56158cbb0a75a8bffa7d3d8e11d4e
uglyhurd <dolgivlad@icloud.com>
2025-12-28 23:41:43 +0300

    First commit

---
+++ src/main/java/com/example/HurdPost/HurdPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.HurdPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurdPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurdPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HurdPost/HurdPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HurdPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HurdPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
