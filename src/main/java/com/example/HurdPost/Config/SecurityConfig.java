2643bea6e732a952edf1ee1c2d3ef1409cc6d62a
uglyhurd <dolgivlad@icloud.com>
2026-01-02 01:18:49 +0300

    Add Configuration

--- src/main/java/com/example/HurdPost/Config/SecurityConfig.java
+++ src/main/java/com/example/HurdPost/Config/SecurityConfig.java
@@ -1,13 +1,56 @@
 package com.example.HurdPost.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.lang.reflect.Executable;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+            authorize -> authorize.requestMatchers("/adminPage").hasRole("ADMIN")
+                    .requestMatchers("/auth/login", "/auth/registration", "/error").anonymous()
+                    .anyRequest().hasAnyRole("ADMIN", "MODERATOR", "USER")
+        ).formLogin(
+                login -> login.loginPage("/auth/login").loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/posts", true)
+                                .failureUrl("/auth/login?error")
+
+        ).logout(
+                logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/auth/login")
+        );
+        return http.build();
+
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder authenticationManagerBuilder =http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(getPasswordEncoder());
+
+        return authenticationManagerBuilder.build();
+
+
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
 }
