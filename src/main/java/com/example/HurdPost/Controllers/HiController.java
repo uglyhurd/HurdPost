fd17ad02ebaa24baeae141976ffd17885d593438
uglyhurd <dolgivlad@icloud.com>
2026-01-26 00:39:23 +0300

    Add Profile

--- src/main/java/com/example/HurdPost/Controllers/HiController.java
+++ src/main/java/com/example/HurdPost/Controllers/HiController.java
@@ -1,16 +1,39 @@
 package com.example.HurdPost.Controllers;
 
+import com.example.HurdPost.Models.User;
+import com.example.HurdPost.Repositories.UserRepos;
+import com.example.HurdPost.Security.PersonDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class HiController {
 
 
 
+    private final UserRepos userRepos;
+    @Autowired
+    public HiController(UserRepos userRepos) {
+        this.userRepos = userRepos;
+    }
+
+
     @GetMapping("/posts")
-    public String LetPosts(){
+    public String LetPosts(Model model,
+                           @AuthenticationPrincipal PersonDetails personDetails){
+
+        Optional<User> optUser = userRepos.findByUsername(personDetails.getUsername());
+        User user = optUser.get();
+//        if(optUser.isEmpty())
+//            model.addAttribute("user", null);
+
+        model.addAttribute("user", user);
         return "auth/posts/posts";
     }
 
