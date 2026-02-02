12edb5e279eaeeb4291b0ee70a3b455be5555d90
uglyhurd <dolgivlad@icloud.com>
2026-02-02 17:49:14 +0300

    AA

--- src/main/java/com/example/HurdPost/Services/UserService.java
+++ src/main/java/com/example/HurdPost/Services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -33,4 +34,13 @@ public class UserService implements UserDetailsService {
 
         return new PersonDetails(user.get());
     }
+
+    @Transactional
+    public User getUserById(int id){
+       Optional<User> user = userRepos.findById(id);
+       if (user.isPresent())
+           return user.get();
+       return new User();
+    }
+
 }
